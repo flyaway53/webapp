9d66f587331ef42e044d7796db11454ac4ea76b6
flyaway53 <13100732@qq.com>
2017-07-05 16:49:39 +0800

    first

---
+++ www/orm.py
@@ -0,0 +1,49 @@
+from orm import Model,StringField,IntegerField
+
+class User(Model):
+    __table__ = 'users'
+
+    id = IntegerField(primary_key = True)
+    name = StringField()
+    
+async def create_pool(loop,**kw):
+    loggong.info('create database connection pool...')
+    global __pool
+    __pool = await aiomysql.create_pool(
+        host = kw.get('host','localhost'),
+        port = kw.get('port',3306),
+        user = kw['root'],
+        password = kw['password'],
+        db = kw['db'],
+        charset = kw.get('charset','utf-8'),
+        autocommit = kw.get('autocommit',True),
+        maxsize = kw.get('maxsize',10),
+        minisize = ke.get('minisize',1),
+        loop = loop
+    )
+async def select(sql,args,size=None):
+    log(sql,args)
+    global __pool
+    with (await __pool) as conn:
+        cur =await conn.cursor(aiomysql.DictCursor)
+        await cur.execute(sql.replace('?',args or ()))
+        if size:
+            rs = await cur.fetchmany(size)
+        else:
+            rs =await cur.fetchall()
+        await cur.close()
+        logging.info('rows returned: %s' % len(rs))
+        return rs
+
+async def execute(sql,args):
+    log(sql)
+    with (await __pool) as conn:
+        try:
+            cur = await conn.cursor()
+            await cur.execute(sql.replace('?','%s'),args)
+            affected = cur.rowcount
+            await cur.close()
+        except BaseExpection as e:
+            raise
+        return affected
+
